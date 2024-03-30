7d5d70bc9cc56c387183519c2e64f0ff58592124
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-03-30 23:09:20 +0700

    feature: add action to button

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ let inventory = ["stick"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+//initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goStore() {
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+
+  function buyHealth() {
+    button1.innerText = "Buy 10 health (10 gold)";
+  }
+  function buyWeapon() {
+    button2.innerText = "Buy weapon (30 gold)";
+  }
+  function goTown() {
+    button3.innerText = "Go to town square";
+  }
+}
+
+function goCave() {
+  button2.innerText = "Buy weapon (30 gold)";
+}
+
+function fightDragon() {
+  button3.innerText = "Go to town square";
+}
