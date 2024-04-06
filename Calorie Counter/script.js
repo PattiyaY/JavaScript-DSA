babded4bf606951c461d77fae70bc0036062dad8
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-06 22:45:24 +0700

    feature: updated addEntry func

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -32,4 +32,7 @@ function isInvalidInput(str) {
 
 function addEntry() {
   const targetId = "#" + entryDropdown.value;
+  const targetInputContainer = document.querySelector(
+    targetId + " .input-container"
+  );
 }
