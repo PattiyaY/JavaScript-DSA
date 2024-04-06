de7745de938e4ee7a91cc136c370f0105ff1aee3
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-06 22:15:31 +0700

    feature: add cleanInputString func

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -4,3 +4,16 @@ const entryDropdown = document.getElementById("entry-dropdown");
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
+
+let isError = false;
+
+function cleanInputString(str) {
+  /* Specified the escape operators
+    \s -> escape white spaces
+    g is a flag, it'll keep looking until the end
+  */
+  const regex = /[+-\s]/g;
+
+  //Replace characters(regex) with ""
+  return str.replace(regex, "");
+}
