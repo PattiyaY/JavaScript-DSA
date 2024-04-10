f7ba4b11ce26d203ee5410353556833ee4654b7d
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-10 20:48:31 +0700

    feature: add calculateCalories func

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -50,6 +50,39 @@ function addEntry() {
   targetInputContainer.insertAdjacentElement("beforeend", HTMLString);
 }
 
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+  // Retrieve all number in input field
+  const breakfastNumberInputs = document.querySelectorAll(
+    "#breakfast input[type=number]"
+  );
+
+  const lunchNumberInputs = document.querySelectorAll(
+    "#lunch input[type=number]"
+  );
+
+  const dinnerNumberInputs = document.querySelectorAll(
+    "#dinner input[type=number]"
+  );
+
+  const snacksNumberInputs = document.querySelectorAll(
+    "#snacks input[type=number]"
+  );
+
+  const exerciseNumberInputs = document.querySelectorAll(
+    "#exercise input[type=number]"
+  );
+
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0;
   for (const item of list) {
