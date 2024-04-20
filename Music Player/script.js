971d047c145bc5aaaffb63d4601ef6342ac8b274
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-20 15:35:09 +0700

    feature: add sortSongs func by name

--- Music Player/script.js
+++ Music Player/script.js
@@ -102,7 +102,24 @@ const renderSongs = (array) => {
         </button>
     </li>`;
     })
-    .join();
+    .join("");
 
   playlistSongs.innerHTML = songsHTML;
 };
+
+const sortSongs = () => {
+  userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+
+    return 0;
+  });
+
+  return userData?.songs;
+};
+
+renderSongs(sortSongs());
