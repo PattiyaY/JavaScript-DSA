9cc6ad495376d680a941a7c094f24789e6093f78
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-22 15:37:53 +0700

    feature: add playNextSong func

--- Music Player/script.js
+++ Music Player/script.js
@@ -107,6 +107,16 @@ const pauseSong = () => {
   audio.pause();
 };
 
+const playNextSong = () => {
+  if (userData?.currentSong === null) {
+    playSong(userData?.songs[0].id);
+  } else {
+    const currentSongIndex = getCurrentSongIndex();
+    const nextSong = userData?.songs[currentSongIndex + 1];
+    playSong(nextSong.id);
+  }
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
@@ -142,6 +152,8 @@ playButton.addEventListener("click", () => {
 });
 
 pauseButton.addEventListener("click", pauseSong);
+
+nextButton.addEventListener("click", playNextSong);
 
 const sortSongs = () => {
   userData?.songs.sort((a, b) => {
