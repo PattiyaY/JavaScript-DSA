d76db71d732b990b3ae617c8fb563a6bca096a1d
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-22 16:15:52 +0700

    feature: add highlightCurrentSong func

--- Music Player/script.js
+++ Music Player/script.js
@@ -98,6 +98,7 @@ const playSong = (id) => {
 
   userData.currentSong = song;
   playButton.classList.add("playing");
+  highlightCurrentSong();
   audio.play();
 };
 
@@ -124,6 +125,21 @@ const playPreviousSong = () => {
     const currentSongIndex = getCurrentSongIndex();
     const previousSong = userData?.songs[currentSongIndex - 1];
     playSong(previousSong.id);
+  }
+};
+
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) {
+    songToHighlight.setAttribute("aria-current", "true");
   }
 };
 
