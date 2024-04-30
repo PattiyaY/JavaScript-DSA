7a76370fbcbad313643ec4b6c46525b35acaaefa
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-04-30 15:38:49 +0700

    feature: Array lessons

--- Introductory JS (pyramid generator)/script.js
+++ Introductory JS (pyramid generator)/script.js
@@ -1,2 +1,21 @@
+//String
 let character = "Hello";
+
+//Number
 let count = 8;
+
+//Array
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+//Update value dynamically access the last element
+rows[rows.length - 1] = 10;
+
+//Add the string "freeCodeCamp" to array
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed);
+
+//Removes the last element from an array
+let popped = rows.pop();
+
+console.log(popped);
+console.log(rows);
