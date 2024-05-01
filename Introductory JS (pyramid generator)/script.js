96ef40b33b327a34e3c3503c2e171178231587c0
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-05-01 22:11:04 +0700

    feature: add for...loop and create padRow func

--- Introductory JS (pyramid generator)/script.js
+++ Introductory JS (pyramid generator)/script.js
@@ -1,21 +1,44 @@
+//can be reassigned
 //String
-let character = "Hello";
+let character_eg = "Hello";
 
 //Number
-let count = 8;
+let count_eg = 8;
 
 //Array
-let rows = ["Naomi", "Quincy", "CamperChan"];
+let rows_eg = ["Naomi", "Quincy", "CamperChan"];
 
 //Update value dynamically access the last element
-rows[rows.length - 1] = 10;
+rows_eg[rows_eg.length - 1] = 10;
 
 //Add the string "freeCodeCamp" to array
-let pushed = rows.push("freeCodeCamp");
+let pushed = rows_eg.push("freeCodeCamp");
 console.log(pushed);
 
 //Removes the last element from an array
-let popped = rows.pop();
+let popped = rows_eg.pop();
 
 console.log(popped);
-console.log(rows);
+console.log(rows_eg);
+
+//Can't be reassigned
+const character = "#";
+const count = 8;
+const rows = [];
+
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(character.repeat(i + 1));
+}
+
+let result = "";
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+function padRow(name) {
+  return "Hello!";
+}
+
+const call = padRow();
+console.log(call);
