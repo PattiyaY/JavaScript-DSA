8177640e3369688a89cf3024cb832a26c385cb3e
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-05-06 21:49:14 +0700

    Fully completed pyramid

--- Introductory JS (pyramid generator)/script.js
+++ Introductory JS (pyramid generator)/script.js
@@ -22,9 +22,10 @@ console.log(popped);
 console.log(rows_eg);
 
 //Can't be reassigned
-const character = "#";
-const count = 8;
+const character = "!";
+const count = 10;
 const rows = [];
+let inverted = false;
 
 function padRow(rowNumber, rowCount) {
   return (
@@ -34,19 +35,42 @@ function padRow(rowNumber, rowCount) {
   );
 }
 
-// TODO: use a different type of loop
-/* for (let i = 1; i <= count; i++) {
-  rows.push(padRow(i, count));
-} */
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
 
-//Pyramid
-while (rows.length <= count) {
+while (rows.length < count) {
   rows.push(padRow(rows.length + 1, count));
 }
 
+for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+}
 let result = "";
 
 for (const row of rows) {
   result = result + "\n" + row;
 }
 console.log(result);
+
+//Upside down pyramid
+/* for (let i = count; i > 0; i--) {
+  rows.push(padRow(i, count));
+} */
+
+/* for (let i = 1; i <= count; i++) {
+  rows.unshift(padRow(i, count));
+} */
+
+/* 
+const numbers = [1, 2, 3];
+const shifted = numbers.shift();
+console.log(shifted);
+const unshifted = numbers.unshift(5);
+console.log(unshifted);
+console.log(numbers);
+*/
