ef79b4c4cbd7d9dbb07621ccd2c04f5dbd3ce896
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-05-07 21:20:02 +0700

    add date formats and complete projects

--- DateFormatter/script.js
+++ DateFormatter/script.js
@@ -16,3 +16,26 @@ const formattedDate = `${day}-${month}-${year}`;
 console.log(formattedDate);
 
 currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate
+        .split("-")
+        .reverse()
+        .join("-");
+      break;
+
+    case "mm-dd-yyyy-h-mm":
+      currentDateParagraph.textContent = `${month}-${day}-${year} ${hours} Hours ${minutes} Minutes`;
+      break;
+
+    default:
+      currentDateParagraph.textContent = formattedDate;
+      break;
+  }
+});
+
+//learn how to work on split() join() and reverse() method.
+const exampleSentence = "selur pmaCedoCeerf".split("").reverse().join("");
+console.log(exampleSentence);
