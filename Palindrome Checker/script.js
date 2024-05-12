c8e33026989b64637609f4a2a4428beef1f7c1ac
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-05-12 22:55:44 +0700

    add checkPalindrome, generateOutput, reverseString and evenlistener

--- Palindrome Checker/script.js
+++ Palindrome Checker/script.js
@@ -1,9 +1,33 @@
 const checkButton = document.getElementById("check-btn");
 const text = document.getElementById("text-input");
 const output = document.getElementById("check");
+const result = document.getElementById("result");
+
+function checkPalindrome(text) {
+  if (text === "") {
+    alert("Please input a value");
+  } else {
+    const reverseText = reverseString(text);
+    if (reverseText === text) {
+      result.innerHTML = generateOutput(text, true);
+    } else {
+      result.innerHTML = generateOutput(text, false);
+    }
+  }
+}
+
+function generateOutput(text, isPalindrome) {
+  if (isPalindrome) {
+    return `${text} is a palindrome`;
+  } else {
+    return `${text} is not a palindrome`;
+  }
+}
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
 checkButton.addEventListener("click", () => {
-  if (text.value === "") {
-    alert("Please input a value");
-  }
+  checkPalindrome(text.value);
 });
