c7dedb6eb3936d7821a24972c54b98e78626b4e7
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-05-16 21:05:28 +0700

    add getGrade func

--- GradeBookApp/script.js
+++ GradeBookApp/script.js
@@ -7,7 +7,29 @@ function getAverage(scores) {
   return totalScores / totalStudents;
 }
 
-function getGrade(score) {}
+function getGrade(score) {
+  if (score === 100) {
+    return "A++";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
+}
+
+function hasPassingGrade(score) {
+  if (score >= 60) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
 console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
@@ -15,3 +37,7 @@ console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
 console.log(getGrade(96));
 console.log(getGrade(82));
 console.log(getGrade(56));
+
+console.log(hasPassingGrade(100));
+console.log(hasPassingGrade(53));
+console.log(hasPassingGrade(87));
