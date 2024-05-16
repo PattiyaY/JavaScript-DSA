32b77c5bef8ba9717bde07cec68fa5008738ff2a
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-05-16 21:16:34 +0700

    fix typo

--- GradeBookApp/script.js
+++ GradeBookApp/script.js
@@ -29,11 +29,11 @@ function hasPassingGrade(score) {
 
 function studentMsg(totalScores, studentScore) {
   if (hasPassingGrade(studentScore)) {
-    return `Class average: ${getAverage(totalScores)} Your grade: ${getGrade(
+    return `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(
       studentScore
     )}. You passed the course.`;
   } else {
-    return `Class average: ${getAverage(totalScores)} Your grade: ${getGrade(
+    return `Class average: ${getAverage(totalScores)}. Your grade: ${getGrade(
       studentScore
     )}. You failed the course.`;
   }
