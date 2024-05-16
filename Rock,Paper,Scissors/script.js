26dcd8c89ac16b86e74985571eda2e531278157e
Pattiya Yiadram <pattiya.2547@icloud.com>
2024-05-16 21:51:57 +0700

    add random and get result func

--- Rock,Paper,Scissors/script.js
+++ Rock,Paper,Scissors/script.js
@@ -1,4 +1,34 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
+  let randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
 console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+  return (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Scissors" && computer === "Paper") ||
+    (player === "Paper" && computer === "Rock")
+  );
+}
+console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
+console.log(getRoundResults("Rock"));
+console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
